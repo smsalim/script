9537eefd65482d3b157f1da8c8cb1d763933a2b7
Mohd Salim <smsalim931@gmail.com>
2025-03-11 00:38:21 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function myFunction(){
-    document.getElementById("index").innerHTML = "Welcome Coder"
-}
+// function myFunction(){
+//     document.getElementById("index").innerHTML = "Welcome Coder"
+// }
+
