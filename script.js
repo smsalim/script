53986f1a97195c4e07ffed46e189bb5ddc79d603
Mohd Salim <smsalim931@gmail.com>
2025-03-19 18:31:46 +0530

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // function myFunction(){
 //     document.getElementById("index").innerHTML = "Welcome Coder"
 // }
+// function myDisplayer(some) {
+//     document.getElementById("demo").innerHTML = some;
+//   }
+  
+//   function myCalculator(num1, num2, myCallback) {
+//     let sum = num1 + num2;
+//     myCallback(sum);
+//   }
+  
+//   myCalculator(5, 5, myDisplayer);
 
-console.log("1")
+  const myDisplayer = (sum)=> {
+    document.getElementById("demo").innerHTML = sum
+  }
 
-setTimeout(()=>{
-    console.log("2")
-}, 2000)
+  const myCalculator = (num1, num2, myCallBack) =>{
+    let sum = num1 + num2
+    myCallBack(sum)
+  }
 
-console.log("3")
+  myCalculator(5, 15, myDisplayer)
